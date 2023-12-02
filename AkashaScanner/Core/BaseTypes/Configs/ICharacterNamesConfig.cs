6908182975258e6a10396b20fb3beab8d7f69636
Craig McIlwrath <craigmc08@gmail.com>
2023-12-02 13:11:55 -0500

    save wanderer name when editing forms (#2)

--- AkashaScanner/Core/BaseTypes/Configs/ICharacterNamesConfig.cs
+++ AkashaScanner/Core/BaseTypes/Configs/ICharacterNamesConfig.cs
@@ -3,5 +3,29 @@
     public interface ICharacterNamesConfig
     {
         Dictionary<string, string> CharacterNameOverrides { get; set; }
+        
+        /// <summary>
+        /// Load a character override name to be used on a display page.
+        /// </summary>
+        /// <param name="character"></param>
+        /// <returns>The override name from `CharacterNameOverrides`, or an
+        /// empty string if there is no name set.</returns>        
+        string getCharacterNameOverride(string character) {
+            return CharacterNameOverrides.GetValueOrDefault(character, "");
+        }
+        
+        /// <summary>
+        /// Set a character override name.
+        /// </summary>
+        /// <param name="character"></param>
+        /// <param name="nameOverrideOrEmpty">The name to use. If empty, the
+        /// character's name override will be removed.</param>
+        void setCharacterNameOverride(string character, string nameOverrideOrEmpty) {
+            if (string.IsNullOrEmpty(nameOverrideOrEmpty)) {
+                CharacterNameOverrides.Remove(character);
+            } else {
+                CharacterNameOverrides[character] = nameOverrideOrEmpty;
+            }
+        }
     }
 }
